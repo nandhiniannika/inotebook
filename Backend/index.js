d5b98cc3ce0abf49e211fb402427197a56275bf1
Annikalla Nandhini <150242225+nandhiniannika@users.noreply.github.com>
2024-12-27 19:39:55 +0530

    first-commit

--- Backend/index.js
+++ Backend/index.js
@@ -1,12 +1,10 @@
 const connectToMongo = require('./db')
 const express = require('express')
-var cors = require('cors')
 
 connectToMongo();
 const port = 5000
 const app = express()
 
-app.use(cors())
 
 app.use(express.json())
 
--- Backend/middleware/fetchuser.js
+++ Backend/middleware/fetchuser.js
@@ -3,7 +3,6 @@ const jwt = require('jsonwebtoken')
 const JWTSecret = process.env.JWT_SECRET
 
 const fetchuser = (req,res,next)=>{
-//Get the user from the jwt token and add id to the req object
 const token = req.header("auth-token")
 if(!token){
     res.status(401).send({error:"Please authenticate using valid token"})
