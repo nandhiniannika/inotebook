ae18582ff90fad314242803980d624b1c74a0211
Annikalla Nandhini <150242225+nandhiniannika@users.noreply.github.com>
2024-12-29 11:33:56 +0530

    first-commit

--- src/index.js
+++ src/index.js
@@ -1,19 +1,33 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
-
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <BrowserRouter> {/* Wrap the App component with BrowserRouter */}
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-);
+// Custom render function
+function customRender(component, container) {
+  // Create a root element if it doesn't exist
+  if (!container) return;
+  
+  // Render the component
+  container.innerHTML = ''; // Clear the container
+  const element = React.createElement(component);
+  React.Children.forEach(element.props.children, child => {
+    const childElement = document.createElement('div');
+    childElement.innerHTML = child;
+    container.appendChild(childElement);
+  });
+}
 
+// Get the root element
+const rootElement = document.getElementById('root');
+
+// Use the custom render function
+customRender(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  rootElement
+);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
